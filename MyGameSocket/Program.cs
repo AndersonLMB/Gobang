5c8ee323f9844dd11b57c8e6ad0e8c939ff96cf0
lmb <1603497362@qq.com>
2017-09-18 17:47:53 +0800

    commit

--- MyGameSocket/Program.cs
+++ MyGameSocket/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MyGameSocket.Game;
+using MyGameSocket.Server;
 
 namespace MyGameSocket
 {
@@ -11,24 +12,77 @@ namespace MyGameSocket
     {
         static void Main(string[] args)
         {
+            GameServer gameServer = new GameServer("ws://127.0.0.1:1836");
+            gameServer.Start();
+            Console.ReadLine();
+
         }
     }
 
 
+
+
     public static class PublicGames
     {
-        public static List<GobangGame> games = new List<GobangGame>(20);
-
-
+        public static List<GobangGame> Games = new List<GobangGame>();
         public static void AddGame()
         {
 
         }
 
-        public static void RemoveGame()
+        public static GobangGame GetGame(int index)
         {
+            return Games[index];
+        }
+        //public static List<GobangGame> games = new List<GobangGame>(20);
+
+
+        //public static void AddGame()
+        //{
+
+        //}
+
+        //public static void RemoveGame()
+        //{
+
+        //}
+
+    }
+
+    public static class OnlinePlayers
+    {
+        public static List<Player> Players = new List<Player>();
+        public static void AddPlayer(Player player)
+        {
+            Players.Add(player);
+            DisplayAllPlayer();
 
         }
+        public static void AddPlayer(string name)
+        {
+            Player player = new Player(name);
+            AddPlayer(player);
+        }
+
+        public static List<Player> GetPlayers()
+        {
+            return Players;
+        }
+
+        public static Player GetPlayer(int index)
+        {
+            return Players[index];
+        }
+
+        public static void DisplayAllPlayer()
+        {
+            for (var i = 0; i < Players.Count; i++)
+            {
+                Console.Write(i.ToString() + ":" + Players[i].Name + " ");
+            }
+            Console.WriteLine();
+        }
+
 
     }
 }
--- MyGameSocket/Server/GameServer.cs
+++ MyGameSocket/Server/GameServer.cs
@@ -10,15 +10,57 @@ using WebSocketSharp.Net.WebSockets;
 using WebSocketSharp.Net;
 using WebSocketSharp.Server;
 using WebSocketSharp;
+using MyGameSocket.Game;
 
 
 namespace MyGameSocket.Server
 {
     class GameServer
     {
-        
+
+        public WebSocketServer WSGameServer;
+
+        public GameServer(string url)
+        {
+            WSGameServer = new WebSocketServer(url);
+            WSGameServer.AddWebSocketService<PlayerLogin>("/PlayerLogin");
+        }
+
+        public void Start()
+        {
+            this.WSGameServer.Start();
+        }
+
 
 
 
     }
+
+    class PlayerLogin : WebSocketBehavior
+    {
+
+        protected override void OnOpen()
+        {
+            base.OnOpen();
+
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            string[] message = e.Data.Split(' ');
+            //Login
+            if (message[0] == "LOGIN")
+            {
+                Player player = new Player(message[1]);
+                OnlinePlayers.AddPlayer(player);
+            }
+            base.OnMessage(e);
+        }
+
+        protected override void OnClose(CloseEventArgs e)
+        {
+            base.OnClose(e);
+        }
+    }
+
 }
